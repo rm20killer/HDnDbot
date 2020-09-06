5830513dcef248fce664a7e02b4edcc9477f1906
rm20killer <43253883+rm20killer@users.noreply.github.com>
2020-09-06 20:23:40 +0100

    bot

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hytale D&D Discord Bot
+
+A simple bot to keep help control the Hytale D&D discord server but also give features like getting the latest blog post on hytale.
---
+++ index.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-inline-comments */
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const HytaleApi = require('hytale-api-sdk');
+const api = new HytaleApi.ArticlesApi();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if(msg.content === '!ping') {
+        msg.reply('pong');
+        console.log('!ping');
+    }
+    if(msg.content === '!madeby') {
+        console.log('!madeby');
+        msg.channel.send('this was made by RM20');
+    }
+    if (msg.content === '!blog') {
+        if (msg.member.roles.cache.find(r => r.name === '⁣       Staff Team       ⁣')) {
+        console.log('!blog');
+
+        // eslint-disable-next-line no-unused-vars
+        api.getArticles({}, function(error, data, response) {
+            if (error) {
+                console.error(error);
+            }
+            else {
+                    const latest = data.slice(0, 1);
+                    latest.forEach(function(article) {
+                        const publishAt = new Date(article.publishedAt);
+                        const embed = new Discord.MessageEmbed()
+                            .setColor(3447003)
+                            .setThumbnail(`https://hytale.com/m/variants/blog_thumb_${article.coverImage.s3Key}`)
+                            .setAuthor('Hytale', 'https://cdn.discordapp.com/attachments/741665332808122469/741685930401857596/G-O9yHTX.jpg')
+                            .setTitle(article.title)
+                            .setDescription(article.bodyExcerpt)
+                            .setURL(`https://hytale.com/news/${publishAt.getFullYear()}/${publishAt.getMonth() + 1}/${article.slug}`)
+                            .addField('Published At', `${publishAt.toLocaleDateString()}`)
+                        ;
+
+                        msg.channel.send(embed);
+                    });
+                }
+            });
+        }
+        else{
+            console.log('!blog user not staff');
+        }
+    }
+});
+
